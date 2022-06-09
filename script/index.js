a69d8639bf5e3e4e62c2fc508de56cdd32239436
citymer <cityhunter8484@gmail.com>
2022-06-09 09:32:02 +0200

    creation h4

--- script/index.js
+++ script/index.js
@@ -1,4 +1,12 @@
-function init() {
-  console.log('hello')
-}
-init()
+const sectionDroite = document.querySelector('#parcours')
+const articleProfil = document.createElement('article')
+sectionDroite.appendChild(articleProfil)
+articleProfil.innerHTML = `<h4>PROFIL PROFESSIONNEL</h4>`
+
+const articleParcours = document.createElement('article')
+sectionDroite.appendChild(articleParcours)
+articleParcours.innerHTML = `<h4>PARCOURS PROFESSIONNEL</h4>`
+
+const articleFormation = document.createElement('article')
+sectionDroite.appendChild(articleFormation)
+articleFormation.innerHTML = `<h4>FORMATION</h4>`
